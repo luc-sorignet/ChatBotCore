294080416ccf66f705d375482135f0960bc3af63
Luc Sorignet <luc.sorignet@gmail.com>
2013-11-14 10:11:40 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+  /***************************************
+  * http://www.program-o.com
+  * PROGRAM O
+  * Version: 2.3.1
+  * FILE: index.php
+  * AUTHOR: Elizabeth Perreau and Dave Morton
+  * DATE: 02-13-2013
+  * DETAILS: Program O's starting point
+  ***************************************/
+
+  if (!file_exists('config/global_config.php'))
+  {
+  # No config exists we will run install
+    header('location: install/install_programo.php');
+  }
+  else
+  {
+    # Config exists we will goto the bot
+    $thisFile = __FILE__;
+    require_once('config/global_config.php');
+    $format = strtoupper($format);
+    switch ($format)
+    {
+      case 'JSON':
+      $gui = 'jquery';
+      break;
+      case 'XML':
+      $gui= 'xml';
+      break;
+      default:
+      $gui = 'plain';
+    }
+    if (!defined('SCRIPT_INSTALLED')) header('location: ' . _INSTALL_URL_ . 'install_programo.php');
+    elseif (file_exists(_INSTALL_PATH_ . 'upgrade.php')) header('Location: ' . _INSTALL_URL_ . 'upgrade.php?returnTo=' . $gui);
+    else header("location: gui/$gui");
+  }
+
+?>
